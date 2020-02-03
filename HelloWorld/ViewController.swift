beb4dd18ad8ebd2114e9bae2617e4739892165a4
yang <heroparkjisung2gmail.com>
2020-02-04 01:30:48 +0900

    add

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
-
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "안녕, " + txtName.text!
+    }
 }
 
